7322e71184289defa2895bf84474211a3c213b8f
man7788 <jerman1841@gmail.com>
2025-07-25 04:37:52 +0800

    create get car list route

--- routes/catalogRouter.js
+++ routes/catalogRouter.js
@@ -6,4 +6,6 @@ const carController = require('../controllers/carController');
 // CAR ROUTES //
 catalogRouter.get('/', carController.getHomeCatalog);
 
+catalogRouter.get('/cars', carController.getCarList);
+
 module.exports = catalogRouter;

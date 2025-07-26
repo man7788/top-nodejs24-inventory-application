9c18247fd15ab27513f3e8dc0c74ea962960debf
man7788 <jerman1841@gmail.com>
2025-07-26 10:12:52 +0800

    create post car update route

--- routes/catalogRouter.js
+++ routes/catalogRouter.js
@@ -14,6 +14,7 @@ catalogRouter.post('/cars/create', carController.postCarCreate);
 catalogRouter.get('/cars/:id', carController.getCarDetail);
 
 catalogRouter.get('/cars/:id/update', carController.getCarUpdate);
+catalogRouter.post('/cars/:id/update', carController.postCarUpdate);
 
 catalogRouter.get('/cars/:id/delete', carController.getCarDelete);
 

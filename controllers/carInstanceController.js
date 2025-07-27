9e789971d62164fc93d5208e2c2f98557e39aa97
man7788 <jerman1841@gmail.com>
2025-07-27 22:59:23 +0800

    create car instance delete controller

--- controllers/carInstanceController.js
+++ controllers/carInstanceController.js
@@ -14,3 +14,20 @@ exports.getCarInstanceDetail = (req, res) => {
     content: content,
   });
 };
+
+exports.getCarInstanceDelete = (req, res) => {
+  content = {
+    id: 1,
+    name: 'Civic',
+    price: 100000,
+    manufacturer: 'Honda',
+    bodyStyle: 'Sedan',
+    productionDate: new Date(),
+    soldDate: new Date(),
+  };
+  res.render('index', {
+    title: 'Delete Car Instance',
+    view: 'carInstances/carinstanceDelete',
+    content: content,
+  });
+};
